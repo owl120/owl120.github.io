17add685a37504863ffb2df48cb0686d1dabdc51
owl120 <msilver129@gmail.com>
2016-09-03 16:03:49 -0500

    Add files via upload

---
+++ upgrade.php
@@ -0,0 +1,11 @@
+﻿<?php 
+
+define('IN_MYBB', 1); require "./global.php";
+
+add_breadcrumb("Upgrade", "upgrade.php"); 
+
+eval("\$html = \"".$templates->get("upgrade_owl")."\";"); 
+
+output_page($html);
+
+?>
